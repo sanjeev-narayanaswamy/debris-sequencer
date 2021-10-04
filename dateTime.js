a7def30fa5231bdd56f375405b8797681cd42ee3
Benjamin Wu <benwu.astro@gmail.com>
2021-10-03 21:35:53 -0400

    added current UTC time for real now

---
+++ dateTime.js
@@ -0,0 +1,22 @@
+import  React, { useState , useEffect } from 'react'
+
+export const DateTime = () => {
+
+    var [date,setDate] = useState(new Date());
+    
+    useEffect(() => {
+        var timer = setInterval(()=>setDate(new Date()), 1000 )
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    
+    });
+
+    return(
+        <div>
+            <p> {date.toUTCString()}</p>
+        </div>
+    )
+}
+
+export default DateTime
